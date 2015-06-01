9e1840d2abc4dfeb487fb540e80cc939ccb07a9a
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-06-01 23:41:00 +0300

    Strip to demo base

--- demo/app.js
+++ demo/app.js
@@ -6,6 +6,9 @@ import Rx from 'rx';
 
 import virtualize from 'vdom-virtualize';
 
+// Other helpers
+import {container$, sequenceCombine$, preloadImage$} from './helpers';
+
 
 // Convenience
 const $Obs = Rx.Observable;
@@ -37,71 +40,21 @@ function searchImages({query, free}) {
 // import {searchImages} from './fake-api';
 
 
-// Other helpers
-function sequenceCombine$(observables$) {
-  // Work around odd behaviour of combineLatest with empty Array
-  // (never yields a value)
-  if (observables$.length === 0) {
-    return Rx.Observable.return([]);
-  } else {
-    return Rx.Observable.combineLatest(observables$, (...all) => all);
-  }
-}
 
-function preloadImage$(src) {
-  const loaded = new Promise((resolve, reject) => {
-    const image = new Image();
-    image.addEventListener('load', resolve);
-    image.addEventListener('error', reject);
-    image.src = src;
-  });
-  return Rx.Observable.fromPromise(loaded);
-}
-
-
-
-
-function container$(tagName, children) {
-  return $Obs.combineLatest(
-    children,
-    (...views) => h(tagName, [...views])
-  );
-}
-
-
-// TODO: split into MVI?
-// TODO: close loop? (V = f(M))
-function input() {
-  const inputted$ = new Rx.Subject;
-  const inputEl = h('input', {
-    type: 'text',
-    placeholder: 'Search images...',
-    oninput: (event) => inputted$.onNext(event)
-  });
-
-  return {
-    model: {
-      value$: inputted$.map(event => event.target.value).startWith('')
-    },
-    tree$: $Obs.return(inputEl)
-  };
-}
-
-
-
-function checkboxEl(checked, changed$) {
+function inputElement(value, changed$) {
   return h('input', {
-    type: 'checkbox',
-    checked: checked,
-    onchange: (event) => changed$.onNext(event)
+    type: 'text',
+    placeholder: 'Enter query...',
+    value: value,
+    oninput: (event) => changed$.onNext(event)
   });
 }
 
-function checkboxView() {
+function inputView() {
   const changed$ = new Rx.Subject;
 
   function render$(model) {
-    return model.checked$.map(checked => checkboxEl(checked, changed$));
+    return model.value$.map(val => inputElement(val, changed$));
   }
 
   return {
@@ -110,118 +63,47 @@ function checkboxView() {
   };
 }
 
-function checkboxIntents(view) {
+function inputIntent(view) {
   return {
-    change$: view.events.changed$.map(event => event.target.checked)
+    update$: view.events.changed$.map(ev => ev.target.value)
   };
 }
 
-function checkboxModel(intents) {
+function inputModel(intent) {
   return {
-    checked$: intents.change$.startWith(false)
+    value$: intent.update$.startWith('')
   };
 }
 
-function checkbox() {
-  const view = checkboxView();
-  const model = checkboxModel(checkboxIntents(view));
+
+function inputComponent() {
+  const view = inputView();
+  const model = inputModel(inputIntent(view));
 
   return {
-    model: {
-      value$: model.checked$
-    },
+    model,
     tree$: view.render$(model)
   };
 }
 
 
-function freeFilter() {
-  const choice = checkbox();
-  const tree$ = choice.tree$.map(tree => h('label', [tree, 'free only']));
 
-  return {
-    model: choice.model,
-    tree$: tree$
-  };
-}
-
-function filtersView() {
-  const queryInput = input();
-  const freeChoice = freeFilter();
+function filtersComponent() {
+  const query = inputComponent();
 
   return {
     model: {
-      query$: queryInput.model.value$,
-      free$:  freeChoice.model.value$
+      query$: query.model.value$
     },
-    // tree$: $Obs.combineLatest(
-    //   queryInput.tree$,
-    //   freeChoice.tree$,
-    //   (queryInputTree, freeChoiceTree) => {
-    //     return h('form', [queryInputTree, freeChoiceTree]);
-    //   })
-    tree$: container$('form', [queryInput.tree$, freeChoice.tree$])
+    tree$: query.tree$
   };
 }
 
-
-function imageCell(src, loaded) {
-  const state = loaded ? 'loaded' : 'loading';
-  return h('span', {className: `image image--${state}`}, [
-    h('img', {src: src})
-  ]);
-}
-
-function resultsComponent({results$, query$}) {
-  const resultsHeader$ = query$.map(query => {
-    return h('h2', `Search results for “${query}”`);
-  });
-
-  const imageList$ = results$.flatMapLatest(results => {
-    const imageEls$ = results.data.map(result => {
-      const imageUrl = result.data.thumbnail.secureUrl;
-
-      return preloadImage$(imageUrl).
-        map(() => imageCell(imageUrl, true)).
-        startWith(imageCell(imageUrl, false));
-    });
-
-    return sequenceCombine$(imageEls$).map(imageEls => {
-      return h('div', imageEls);
-    });
-  });
-
-  const tree$ = container$('div', [
-    resultsHeader$,
-    imageList$
-  ]);
-
-  return {
-    tree$
-  };
-}
 
 function view() {
-  const filters = filtersView();
+  const filters = filtersComponent();
 
-  const query$ = filters.model.query$.debounce(500);
-  const free$ = filters.model.free$;
-
-  const pollPeriod = 10000;
-
-  const searchQuery$ = Rx.Observable.combineLatest(
-    query$,
-    free$,
-    $Obs.timer(0, pollPeriod), // emits to refresh
-    (query, free) => ({query, free})
-  );
-
-  const results$ = searchQuery$.
-          flatMapLatest(searchQuery => searchImages(searchQuery));
-
-  const results = resultsComponent({query$, results$});
-
-  const tree$ = container$('div', [filters.tree$, results.tree$]);
+  const tree$ = filters.tree$;
 
   return {
     tree$
@@ -238,6 +120,7 @@ const theView = view();
 theView.tree$.
     startWith(initialDom).
     bufferWithCount(2, 1).
+    filter(pair => pair.length == 2).
     map(([last, current]) => diff(last, current)).
     reduce((out, patches) => patch(out, patches), out).
     subscribeOnError(err => console.error(err));
---
+++ demo/helpers.js
@@ -0,0 +1,33 @@
+import h from 'virtual-dom/h';
+import Rx from 'rx';
+
+
+// Convenience
+const $Obs = Rx.Observable;
+
+export function container$(tagName, children) {
+  return $Obs.combineLatest(
+    children,
+    (...views) => h(tagName, [...views])
+  );
+}
+
+function sequenceCombine$(observables$) {
+  // Work around odd behaviour of combineLatest with empty Array
+  // (never yields a value)
+  if (observables$.length === 0) {
+    return Rx.Observable.return([]);
+  } else {
+    return Rx.Observable.combineLatest(observables$, (...all) => all);
+  }
+}
+
+function preloadImage$(src) {
+  const loaded = new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener('load', resolve);
+    image.addEventListener('error', reject);
+    image.src = src;
+  });
+  return Rx.Observable.fromPromise(loaded);
+}
